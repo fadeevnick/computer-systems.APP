659411023876ae583d1cff61d3f794c947d9d9a3
Nickf <fadeev1999nikolay@gmail.com>
2025-01-30 15:13:24 +0300

    log some tests

--- problem2.27.c
+++ problem2.27.c
@@ -2,10 +2,13 @@
 #include <limits.h>
 
 int uadd_ok(unsigned x, unsigned y) {
-  return x + y < x;
+  return x + y < x ? 0 : 1;
 }
 
 int main() {
   printf("%d\n", sizeof(unsigned));
   printf("%u\n", UINT_MAX);
+
+  printf("%d\n", uadd_ok(4, 4));
+  printf("%d\n", uadd_ok(4, UINT_MAX));
 }
