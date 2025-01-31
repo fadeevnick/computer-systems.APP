532e90961b99955f67bc7d3a83df13533e29f31b
Nickf <fadeev1999nikolay@gmail.com>
2025-01-31 20:26:57 +0300

    add problem2.36

---
+++ problem2.36.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+
+int tmult_ok(int x, int y) {
+  int64_t a = x;
+  int64_t b = y;
+
+  int64_t p = a * b;
+  printf("%d\n", p);
+
+  if (p >= 0) {
+    return p <= INT_MAX;
+  } else {
+    return p >= INT_MIN;
+  }
+}
+
+int main() {
+  // 0
+  printf("%d\n", tmult_ok(2000000000, 20));
+
+  // 1
+  printf("%d\n", tmult_ok(1000, 2));
+}
