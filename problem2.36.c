19d660aaa687696cd0ac26bd93cd68c4e0752420
Nickf <fadeev1999nikolay@gmail.com>
2025-02-03 18:21:36 +0300

    .

--- problem2.36.c
+++ problem2.36.c
@@ -18,7 +18,7 @@ int tmult_ok(int x, int y) {
 
 int main() {
   // 0
-  printf("%d\n", tmult_ok(2000000000, 20));
+  printf("%d\n", tmult_ok(1000000000, 20));
 
   // 1
   printf("%d\n", tmult_ok(1000, 2));
